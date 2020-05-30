eb028d589310f226490872a15630e80cd340eec1
SaidBySolo <saidbysolo@gmail.com>
2020-05-30 18:19:28 +0900

    Updates

---
+++ precache-manifest.261117da4e952e28a52db2e352e633ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ab0eb657418ddcb570ab1172f69772",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "60d92c0233d008402ce6",
+    "url": "/portfolio/static/css/2.1a85028f.chunk.css"
+  },
+  {
+    "revision": "1cce5807c89f7d04cb94",
+    "url": "/portfolio/static/css/main.e64bcb11.chunk.css"
+  },
+  {
+    "revision": "60d92c0233d008402ce6",
+    "url": "/portfolio/static/js/2.843f0437.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolio/static/js/2.843f0437.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1cce5807c89f7d04cb94",
+    "url": "/portfolio/static/js/main.6aa9f773.chunk.js"
+  },
+  {
+    "revision": "81119880bf0581e7b1af",
+    "url": "/portfolio/static/js/runtime-main.ec0e67f1.js"
+  }
+]);
