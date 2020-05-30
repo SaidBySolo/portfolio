aa739ede4e4489a5fe56accad2e3b386cad85984
SaidBySolo <saidbysolo@gmail.com>
2020-05-30 22:43:38 +0900

    Complete make my portfolio web

--- src/Home/data.source.js
+++ src/Home/data.source.js
@@ -1,15 +1,17 @@
 import React from 'react';
 export const Banner00DataSource = {
-  wrapper: { className: 'banner0 kasv2tnm4h-editor_css' },
-  textWrapper: { className: 'banner0-text-wrapper kasw16oszm-editor_css' },
+  wrapper: { className: 'banner0 kati8bjqhxh-editor_css' },
+  textWrapper: { className: 'banner0-text-wrapper' },
   title: {
-    className: 'banner0-title kasv452pwsi-editor_css',
+    className: 'banner0-title kati9wbnx2n-editor_css',
     children: (
       <span>
         <span>
           <span>
-            <p>코딩을 잘하지는 못하지만</p>
-            <p>저는 계속 노력합니다.</p>
+            <b>
+              <p>코딩을 잘하지는 못하지만</p>
+              <p>저는 계속 노력합니다.</p>
+            </b>
           </span>
         </span>
       </span>
@@ -25,11 +27,59 @@ export const Banner00DataSource = {
   },
   button: { className: 'banner0-button', children: 'Learn More' },
 };
+export const Feature10DataSource = {
+  wrapper: {
+    className: 'home-page-wrapper content1-wrapper katluye4tc6-editor_css',
+  },
+  OverPack: { className: 'home-page content1', playScale: 0.3 },
+  imgWrapper: { className: 'content1-img', md: 10, xs: 24 },
+  img: {
+    children: 'https://avatars1.githubusercontent.com/u/33891531?s=400&v=4',
+  },
+  textWrapper: { className: 'content1-text', md: 14, xs: 24 },
+  title: {
+    className: 'content1-title katltjmj7k-editor_css',
+    children: (
+      <span>
+        <span>
+          <span>
+            <span>
+              <p>제 이름은 류주헌 입니다.</p>
+            </span>
+          </span>
+        </span>
+      </span>
+    ),
+  },
+  content: {
+    className: 'content1-content katlvx79oqp-editor_css',
+    children: (
+      <span>
+        <span>
+          <span>
+            <p>보통 라고솔로가말했습니다(SaidBySolo)로 불립니다.</p>
+            <p>아무런 의미가 없는 닉네임처럼 보이지만,</p>
+            <p>
+              모태솔로니 사귀어보고 한번이라도 차여보자! 라는 뜻을 가지고
+              있답니다!
+            </p>
+            <p>
+              <br />
+            </p>
+            <p>대한민국 인천에 서식하고 있어요.</p>
+            <p>현재 인천기계공업고등학교 전기과를 재학중이지만,</p>
+            <p>따라는 자격증은 안따고 파이썬만 끄젹이고 있는 학생이에요</p>
+          </span>
+        </span>
+      </span>
+    ),
+  },
+};
 export const Content00DataSource = {
   wrapper: {
-    className: 'home-page-wrapper content0-wrapper kasvdzyj8l-editor_css',
+    className: 'home-page-wrapper content0-wrapper katic6shd2i-editor_css',
   },
-  page: { className: 'home-page content0 kasvcwy8ty-editor_css' },
+  page: { className: 'home-page content0' },
   OverPack: { playScale: 0.3, className: '' },
   titleWrapper: {
     className: 'title-wrapper',
@@ -41,7 +91,7 @@ export const Content00DataSource = {
             <p>할수있는것들!</p>
           </span>
         ),
-        className: 'kasvb4njo3l-editor_css',
+        className: 'kati5vvomyg-editor_css',
       },
     ],
   },
@@ -64,7 +114,7 @@ export const Content00DataSource = {
             },
             {
               name: 'title',
-              className: 'content0-block-title kasvhvvupz-editor_css',
+              className: 'content0-block-title katil0wjy0t-editor_css',
               children: (
                 <span>
                   <p>파이썬</p>
@@ -78,7 +128,7 @@ export const Content00DataSource = {
                   <p>그나마 자신있고 능숙하게 다루는 언어에요!</p>
                 </span>
               ),
-              className: 'kasvnhkf46m-editor_css',
+              className: 'katimc1d43g-editor_css',
             },
           ],
         },
@@ -99,7 +149,7 @@ export const Content00DataSource = {
             },
             {
               name: 'title',
-              className: 'content0-block-title kasvi6owicc-editor_css',
+              className: 'content0-block-title katilsycvrt-editor_css',
               children: (
                 <span>
                   <p>자바스크립트</p>
@@ -110,12 +160,12 @@ export const Content00DataSource = {
               name: 'content',
               children: (
                 <span>
-                  <span>
-                    <p>포트폴리오 를 만들면서 조금 알게되었어요!</p>
-                  </span>
+                  <p>
+                    포트폴리오를 만들면서 조금 알게되었어요!<br />
+                  </p>
                 </span>
               ),
-              className: 'kasvnegzr7-editor_css',
+              className: 'katimen8oyl-editor_css',
             },
           ],
         },
@@ -136,7 +186,7 @@ export const Content00DataSource = {
             },
             {
               name: 'title',
-              className: 'content0-block-title kasvid4yz2h-editor_css',
+              className: 'content0-block-title katilxkhwbf-editor_css',
               children: (
                 <span>
                   <p>리액트</p>
@@ -147,15 +197,358 @@ export const Content00DataSource = {
               name: 'content',
               children: (
                 <span>
-                  <p>자바스크립트처럼 포트폴리오 만들떄</p>
-                  <p>털끝정도(?) 건드렸어요!</p>
+                  <span>
+                    <span>
+                      <span>
+                        <p>자바스크립트처럼 포트폴리오를 만들때</p>
+                        <p>털끝정도(?) 건드렸어요!</p>
+                      </span>
+                    </span>
+                  </span>
                 </span>
               ),
-              className: 'kasvnb1oglp-editor_css',
+              className: 'katimhf2fqd-editor_css',
             },
           ],
         },
       },
     ],
+  },
+};
+export const Feature40DataSource = {
+  wrapper: {
+    className: 'home-page-wrapper content6-wrapper katmz7ip5k-editor_css',
+  },
+  OverPack: { className: 'home-page content6' },
+  textWrapper: { className: 'content6-text', xs: 24, md: 10 },
+  titleWrapper: {
+    className: 'title-wrapper',
+    children: [
+      {
+        name: 'title',
+        children: (
+          <span>
+            <p>진행했던 프로젝트들!</p>
+          </span>
+        ),
+        className: 'title-h1 katn0bcave7-editor_css',
+      },
+      {
+        name: 'content',
+        className: 'title-content katmyxivhge-editor_css',
+        children: (
+          <span>
+            <span>
+              <p>제 프로젝트들은 깃허브와 함께했습니다.</p>
+            </span>
+          </span>
+        ),
+      },
+    ],
+  },
+  img: {
+    children: 'https://i.imgur.com/wZ8oqvB.png',
+    className: 'content6-img katmwpcwkct-editor_css',
+    xs: 24,
+    md: 14,
+  },
+  block: {
+    children: [
+      {
+        name: 'block0',
+        img: {
+          children:
+            'https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg',
+          className: 'content6-icon',
+        },
+        title: {
+          className: 'content6-title katn0inbcg-editor_css',
+          children: (
+            <span>
+              <span>
+                <p>
+                  <a href="https://github.com/SaidBySolo/neispy">Neispy</a>
+                </p>
+              </span>
+            </span>
+          ),
+        },
+        content: {
+          className: 'content6-content katmygvop5-editor_css',
+          children: (
+            <span>
+              <span>
+                <p>
+                  <a href="https://open.neis.go.kr/portal/mainPage.do">
+                    open neis api
+                  </a>를 파이썬으로 래핑하였습니다.
+                </p>
+                <p>디스코드 급식봇을 만들려다가 제작하였습니다.</p>
+                <p>비동기와 동기 둘다 지원합니다.</p>
+              </span>
+            </span>
+          ),
+        },
+      },
+      {
+        name: 'block1',
+        img: {
+          className: 'content6-icon',
+          children:
+            'https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg',
+        },
+        title: {
+          className: 'content6-title katn0nsx5u-editor_css',
+          children: (
+            <span>
+              <span>
+                <p class="">
+                  <a href="https://github.com/SaidBySolo/DBKR-API-Python">
+                    DBKR-API-Python
+                  </a>
+                </p>
+              </span>
+            </span>
+          ),
+        },
+        content: {
+          className: 'content6-content katmybh45vo-editor_css',
+          children: (
+            <span>
+              <span>
+                <p>
+                  <a href="https://koreanbots.dev/">koreanbots</a>의 api를
+                  래핑하였습니다.
+                </p>
+                <p>CBT때 할게없어서 래핑해보았습니다.</p>
+              </span>
+            </span>
+          ),
+        },
+      },
+      {
+        name: 'block2',
+        img: {
+          className: 'content6-icon',
+          children:
+            'https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg',
+        },
+        title: {
+          className: 'content6-title katn0u0jo3a-editor_css',
+          children: (
+            <span>
+              <p>
+                히요비관련(<a href="https://github.com/SaidBySolo/Hiyobi-Downloder">
+                  다운로더
+                </a>,<a href="https://github.com/SaidBySolo/HiyobiPy">래퍼</a>,<a href="https://github.com/SaidBySolo/Hiyobot">
+                  디스코드봇
+                </a>)
+              </p>
+            </span>
+          ),
+        },
+        content: {
+          className: 'content6-content katmylbuy4a-editor_css',
+          children: (
+            <span>
+              <p>갑자기 아이디어가 떠올라</p>
+              <p>먼저 디스코드봇을 첫번째로 만들었습니다.</p>
+              <p>그후에 다운로더,래퍼 등을 만들었습니다.</p>
+            </span>
+          ),
+        },
+      },
+    ],
+  },
+};
+export const Footer10DataSource = {
+  wrapper: { className: 'home-page-wrapper footer1-wrapper' },
+  OverPack: { className: 'footer1', playScale: 0.2 },
+  block: {
+    className: 'home-page',
+    gutter: 0,
+    children: [
+      {
+        name: 'block0',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: {
+          className: 'logo',
+          children: (
+            <span>
+              <p>SaidBySolo</p>
+            </span>
+          ),
+        },
+        childWrapper: {
+          className: 'slogan',
+          children: [
+            {
+              name: 'content0',
+              children: (
+                <span>
+                  <p>라고솔로가말했습니다</p>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+      {
+        name: 'block1',
+        xs: 24,
+        md: 6,
+        className: 'block katnn9gnfa8-editor_css',
+        title: {
+          children: (
+            <span>
+              <p>Contact</p>
+            </span>
+          ),
+        },
+        childWrapper: {
+          children: [
+            {
+              name: 'link0',
+              href: 'https://github.com/saidbysolo',
+              children: (
+                <span>
+                  <p>Github</p>
+                </span>
+              ),
+            },
+            {
+              name: 'link1',
+              href: 'https://discord.gg/uFTNBfC',
+              children: (
+                <span>
+                  <p>Discord</p>
+                </span>
+              ),
+              className: 'katnnism28d-editor_css',
+            },
+            {
+              name: 'link2',
+              href: 'mailto:saidbysolo@gmai.com',
+              children: (
+                <span>
+                  <p>Email</p>
+                </span>
+              ),
+            },
+            {
+              name: 'link3',
+              href: 'https://steamcommunity.com/id/saidbysolo',
+              children: (
+                <span>
+                  <p>Steam</p>
+                </span>
+              ),
+              className: 'katnp681b8d-editor_css',
+            },
+          ],
+        },
+      },
+      {
+        name: 'block2',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: {
+          children: (
+            <span>
+              <p>
+                Library used<br />
+              </p>
+            </span>
+          ),
+        },
+        childWrapper: {
+          children: [
+            {
+              href: 'https://github.com/facebook/create-react-app',
+              name: 'link0',
+              children: (
+                <span>
+                  <p>create-react-app</p>
+                </span>
+              ),
+            },
+            {
+              href: 'https://ant.design/',
+              name: 'link1',
+              children: (
+                <span>
+                  <span>
+                    <span>
+                      <p>Ant Design</p>
+                    </span>
+                  </span>
+                </span>
+              ),
+            },
+            {
+              href: 'https://landing.ant.design/',
+              name: 'link~katln4uzqwu',
+              children: (
+                <span>
+                  <p>Ant Desgin Landing</p>
+                </span>
+              ),
+            },
+            {
+              href: 'https://motion.ant.design/',
+              name: 'link~katln68onx',
+              children: (
+                <span>
+                  <span>
+                    <p>Ant Motion</p>
+                  </span>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+      {
+        name: 'block3',
+        xs: 24,
+        md: 6,
+        className: 'block',
+        title: {
+          children: (
+            <span>
+              <p>Imege link</p>
+            </span>
+          ),
+        },
+        childWrapper: {
+          children: [
+            {
+              href: 'https://www.pixiv.net/artworks/74324801',
+              name: 'link0',
+              children: (
+                <span>
+                  <p>Pixiv</p>
+                </span>
+              ),
+            },
+          ],
+        },
+      },
+    ],
+  },
+  copyrightWrapper: { className: 'copyright-wrapper' },
+  copyrightPage: { className: 'home-page' },
+  copyright: {
+    className: 'copyright',
+    children: (
+      <span>
+        ©2020 by{' '}
+        <a href="https://github.com/SaidBySolo/portfolio">SaidBySolo</a>&nbsp;All
+        Rights Reserved
+      </span>
+    ),
   },
 };
--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -4,10 +4,17 @@ import React from 'react';
 import { enquireScreen } from 'enquire-js';
 
 import Banner0 from './Banner0';
+import Feature1 from './Feature1';
 import Content0 from './Content0';
+import Feature4 from './Feature4';
+import Footer1 from './Footer1';
+
 import {
   Banner00DataSource,
+  Feature10DataSource,
   Content00DataSource,
+  Feature40DataSource,
+  Footer10DataSource,
 } from './data.source';
 import './less/antMotionStyle.less';
 
@@ -53,10 +60,28 @@ export default class Home extends React.Component {
         dataSource={Banner00DataSource}
         isMobile={this.state.isMobile}
       />,
+      <Feature1
+        id="Feature1_0"
+        key="Feature1_0"
+        dataSource={Feature10DataSource}
+        isMobile={this.state.isMobile}
+      />,
       <Content0
         id="Content0_0"
         key="Content0_0"
         dataSource={Content00DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Feature4
+        id="Feature4_0"
+        key="Feature4_0"
+        dataSource={Feature40DataSource}
+        isMobile={this.state.isMobile}
+      />,
+      <Footer1
+        id="Footer1_0"
+        key="Footer1_0"
+        dataSource={Footer10DataSource}
         isMobile={this.state.isMobile}
       />,
     ];
